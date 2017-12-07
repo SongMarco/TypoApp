3253db56ed98f44079af70a9394fddde48ccef55
jamsya <jamsya@naver.com>
2017-12-08 01:33:21 +0900

    ui 카드뷰 개선 완료

--- app/src/main/java/nova/typoapp/NewsFeedFragment.java
+++ app/src/main/java/nova/typoapp/NewsFeedFragment.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -76,7 +76,7 @@ public class NewsFeedFragment extends Fragment {
     RecyclerView recyclerView;
 
     @BindView(R.id.layoutAdd)
-    LinearLayout layoutAdd;
+    CardView layoutAdd;
 
 
     String json_result;
